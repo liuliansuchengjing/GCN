2f87d07990a51c6647592bd34cd18781d2291126
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-06-20 10:47:24 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -68,8 +68,7 @@ class HGNN2(nn.Module):
         self.fc1 = nn.Linear(emb_dim, emb_dim, bias=False)
         self.fc2 = nn.Linear(emb_dim, emb_dim, bias=False)
         self.weight = nn.Parameter(torch.Tensor(emb_dim, emb_dim))
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_channels))
+        self.bias = nn.Parameter(torch.Tensor(emb_dim))
 
     def forward(self, x, G):
         final = [x]
@@ -87,34 +86,34 @@ class HGNN2(nn.Module):
         x = F.softmax(x,dim = 1)
         return x
 
-class DJconv(nn.Module):
-    def __init__(self, in_channels, out_channels, layers = 1, bias=True):
-        super(DJconv, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
+# class DJconv(nn.Module):
+#     def __init__(self, in_channels, out_channels, layers = 1, bias=True):
+#         super(DJconv, self).__init__()
+#         self.in_channels = in_channels
+#         self.out_channels = out_channels
 
-        self.weight = nn.Parameter(torch.Tensor(in_channels, out_channels))
-        # use xavier initialization
-        nn.init.xavier_uniform_(self.weight)
-        if bias:
-            self.bias = nn.Parameter(torch.Tensor(out_channels))
-        self.layers = layers
+#         self.weight = nn.Parameter(torch.Tensor(in_channels, out_channels))
+#         # use xavier initialization
+#         nn.init.xavier_uniform_(self.weight)
+#         if bias:
+#             self.bias = nn.Parameter(torch.Tensor(out_channels))
+#         self.layers = layers
 
 
-    def forward(self, H, U):
-        adj = torch.matmul(H, H.t())
-        item_embeddings = U
-        item_embedding_layer0 = item_embeddings
-        final = [item_embedding_layer0]
-        for i in range(self.layers):
-            item_embeddings = torch.sparse.mm(adj.cuda(), item_embeddings)
-            final.append(item_embeddings)
-        #  final1 = trans_to_cuda(torch.tensor([item.cpu().detach().numpy() for item in final]))
-        #  item_embeddings = torch.sum(final1, 0)
-        final_tensor = torch.stack(final, dim=0)  # 将列表转换为张量列表（在额外的维度上）  
-        item_embeddings = final_tensor.sum(dim=0) / (self.layers + 1)  # 求和并平均
-        item_embeddings = torch.matmul(item_embeddings, self.weight) + self.bias
-        return item_embeddings
+#     def forward(self, H, U):
+#         adj = torch.matmul(H, H.t())
+#         item_embeddings = U
+#         item_embedding_layer0 = item_embeddings
+#         final = [item_embedding_layer0]
+#         for i in range(self.layers):
+#             item_embeddings = torch.sparse.mm(adj.cuda(), item_embeddings)
+#             final.append(item_embeddings)
+#         #  final1 = trans_to_cuda(torch.tensor([item.cpu().detach().numpy() for item in final]))
+#         #  item_embeddings = torch.sum(final1, 0)
+#         final_tensor = torch.stack(final, dim=0)  # 将列表转换为张量列表（在额外的维度上）  
+#         item_embeddings = final_tensor.sum(dim=0) / (self.layers + 1)  # 求和并平均
+#         item_embeddings = torch.matmul(item_embeddings, self.weight) + self.bias
+#         return item_embeddings
 
 
 def get_previous_user_mask(seq, user_size):
@@ -206,9 +205,9 @@ class HGNN_ATT(nn.Module):
             self.batch_norm1 = torch.nn.BatchNorm1d(output_size)
         self.gat1 = HGATLayer(input_size, output_size, dropout=self.dropout, transfer=False, concat=True, edge=True)
         self.fus1 = Fusion(output_size)
-        self.hgnn = DJconv(64, 64, 1)
+        # self.hgnn = DJconv(64, 64, 1)
         # self.hgnn = HGNN_conv(input_size, output_size, True)
-        # self.hgnn = HGNN2(input_size, 0.3)
+        self.hgnn = HGNN2(input_size, 0.3)
 
     def forward(self, x, hypergraph_list):
         root_emb = F.embedding(hypergraph_list[1].cuda(), x)
