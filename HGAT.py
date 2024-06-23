9a93e0a2dadd9c1e0db805b76f387efbe3f720fb
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-06-23 15:26:15 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -333,7 +333,7 @@ class MSHGAT(nn.Module):
         
         GRUoutput, h = self.GRU(cas_emb_sta, h)   
         # dy_output = GRUoutput.sum(dim=1)  
-        print("GRUoutput.shape:", GRUoutput.size())
+        
         output =  self.fus2(dy_emb_,GRUoutput)
         pred = self.pred(output)
         # print("pred.shape:", pred.size())
