804b8d054d1882eb92c32755bd28410d3a52883f
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-06-23 23:01:43 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -248,7 +248,7 @@ class MSHGAT(nn.Module):
         self.embedding = nn.Embedding(self.n_node, self.initial_feature, padding_idx=0)
         self.reset_parameters()
         self.readout = MLPReadout(self.hidden_size, self.n_node, None)
-        self.GRU = GRUNet(self.hidden_size, self.hidden_size, self.hidden_size, 3)
+        self.GRU = GRUNet(self.hidden_size, self.hidden_size, self.hidden_size, 4)
 
     def reset_parameters(self):
         stdv = 1.0 / math.sqrt(self.hidden_size)
