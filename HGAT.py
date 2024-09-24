631c8eeb77b08ddc2ea02521b59d6c2310f930bd
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-09-24 20:10:49 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -302,20 +302,10 @@ class MSHGAT(nn.Module):
                 dyemb += sub_emb
                 cas_emb += sub_cas
 
-        item_emb1 = dyemb
-        input_emb1 = item_emb1 + cas_emb
-        input_emb1 = self.LayerNorm(input_emb1)
-        input_emb1 = self.dropout(input_emb1)
-
-        position_ids = torch.arange(input.size(1), dtype=torch.long, device=input.device)
-        position_ids = position_ids.unsqueeze(0).expand_as(input)
-        position_embedding = self.position_embedding(position_ids.cuda())
-        # item_emb2 = self.item_embedding(input.cuda())
-        item_emb2 = all_emb
-        input_emb2 = item_emb2 + position_embedding
-        input_emb2 = self.LayerNorm(input_emb2)
-        input_emb2 = self.dropout(input_emb2)
-        input_emb = self.fus(input_emb1, input_emb2)
+        item_emb = dyemb
+        input_emb = item_emb + cas_emb
+        input_emb = self.LayerNorm(input_emb)
+        input_emb = self.dropout(input_emb)
         extended_attention_mask = self.get_attention_mask(input)
         trm_output = self.trm_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=False)
 
