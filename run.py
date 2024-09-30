88b8e953648b7f7140db424f9b9e1fa9af6e607d
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-09-30 15:01:21 +0800

    Update run.py

--- run.py
+++ run.py
@@ -28,8 +28,8 @@ metric = Metrics()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-data_name', default='MOOCCube10000')
-parser.add_argument('-epoch', type=int, default=120)
+parser.add_argument('-data_name', default='r_MOOC10000')
+parser.add_argument('-epoch', type=int, default=60)
 parser.add_argument('-batch_size', type=int, default=64)
 parser.add_argument('-d_model', type=int, default=64)
 parser.add_argument('-initialFeatureSize', type=int, default=64)
@@ -214,6 +214,6 @@ def test_model(MSHGAT, data_path):
 
 if __name__ == "__main__": 
     model = MSHGAT  
-    # train_model(model, opt.data_name)
-    test_model(model, opt.data_name)
+    train_model(model, opt.data_name)
+    # test_model(model, opt.data_name)
 
