6f11d4288bd91321f88a95af4d2cd9946a535efa
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-10-02 15:56:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -206,7 +206,7 @@ def test_model(MSHGAT, data_path):
     model.load_state_dict(torch.load(opt.save_path))
     model.cuda()
 
-    scores = test_epoch(model, test_data, relation_graph, hypergraph_list)
+    scores = test_epoch_pro(model, test_data, relation_graph, hypergraph_list)
     print('  - (Test) ')
     for metric in scores.keys():
         print(metric + ' ' + str(scores[metric]))
