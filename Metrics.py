8952d1af210561dd9fc6e7a7511134c2adca7070
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-10-17 09:56:26 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -106,6 +106,7 @@ class Metrics(object):
         scores = {k: np.mean(v) for k, v in scores.items()}
         return scores, scores_len
 
+    @staticmethod
     def get_courses_by_video(self, video_name, course_video_mapping):
         """根据视频名称获取其所属的课程"""
         courses = []
@@ -127,14 +128,13 @@ class Metrics(object):
         # 加载数据
         idx2u = load_idx2u()
         u2idx = load_u2idx()
-        course_video = load_course_video()
         courses = load_course()
 
         scores = {f'hits@{k}': [] for k in k_list}
         scores.update({f'map@{k}': [] for k in k_list})
 
         # 提取课程对应视频的映射关系
-        course_video_mapping = self.build_course_video_mapping(courses, course_video)
+        course_video_mapping = self.build_course_video_mapping(courses)
 
         for p_, y_, y_p, wc, dt, wt, d1, d2, d3 in zip(y_prob, y_true, y_prev, w_c, d_t, w_t, d_1, d_2, d_3):
             if y_ == self.PAD:
@@ -144,10 +144,15 @@ class Metrics(object):
             top20 = self.get_top_k_predictions(p_, k=20)
 
             # 计算预测视频的分数
-            scores_pro = self.score_predictions(top20, y_p, idx2u, u2idx, course_video_mapping, courses)
+            scores_pro, f_next_video = self.score_predictions(top20, y_p, idx2u, course_video_mapping, courses)
+
+            if f_next_video:
+                # 通过前一个视频找到相邻的下一个视频
+                prev_video_name = idx2u[y_p]
+                next_video_id = self.find_next_video(prev_video_name, course_video_mapping, u2idx, courses)
 
             # 根据得分重新排序top20
-            sorted_top20 = self.reorder_top_predictions(top20, scores_pro)
+            sorted_top20 = self.reorder_top_predictions(top20, scores_pro, next_video_id)
 
             # 更新结果
             for k in k_list:
@@ -158,7 +163,26 @@ class Metrics(object):
         scores = {k: np.mean(v) for k, v in scores.items()}
         return scores, scores_len
 
-    def build_course_video_mapping(self, courses, course_video):
+    def find_next_video(self, prev_video_name, course_video_mapping, u2idx, courses):
+        """在课程中找到相邻的下一个视频"""
+        prev_courses = self.get_courses_by_video(prev_video_name, course_video_mapping)
+        for course_id in prev_courses:
+            for course in courses:
+                if course['id'] == course_id:
+                    video_order = course.get('video_order', [])
+                    try:
+                        y_index = video_order.index(prev_video_name)
+                        # 如果下一个视频存在，返回它的ID
+                        if y_index + 1 < len(video_order):
+                            next_video_name = video_order[y_index + 1]
+                            if next_video_name in u2idx:
+                                return u2idx[next_video_name]
+                    except ValueError:
+                        continue
+        return None
+
+    @staticmethod
+    def build_course_video_mapping(self, courses):
         """构建课程与视频的映射关系，减少重复查找"""
         mapping = {}
         for course in courses:
@@ -167,11 +191,12 @@ class Metrics(object):
             mapping[course_id] = video_order
         return mapping
 
+    @staticmethod
     def get_top_k_predictions(self, p_, k=20):
         """获取排序后的前K个预测视频"""
         return p_.argsort()[::-1][:k]
 
-    def score_predictions(self, top20, y_p, idx2u, u2idx, course_video_mapping, courses):
+    def score_predictions(self, top20, y_p, idx2u, course_video_mapping, courses):
         """根据与历史视频的关联性给每个预测视频评分"""
         scores_pro = {video_id: 0 for video_id in top20}
         prev_video_name = idx2u[y_p]
@@ -182,16 +207,18 @@ class Metrics(object):
             prev_courses = self.get_courses_by_video(prev_video_name, course_video_mapping)
 
             # 计算距离评分
-            scores_pro[video_id] += self.calculate_distance_score(predicted_courses, prev_courses, courses,
-                                                                  prev_video_name, predicted_video_name, u2idx)
+            score, f_next_video = self.calculate_distance_score(predicted_courses, prev_courses, courses,
+                                                                  prev_video_name, predicted_video_name)
 
-        return scores_pro
+            scores_pro[video_id] += score
 
-    def calculate_distance_score(self, predicted_courses, prev_courses, courses, prev_video_name, predicted_video_name,
-                                 u2idx):
+        return scores_pro, f_next_video
+
+    @staticmethod
+    def calculate_distance_score(self, predicted_courses, prev_courses, courses, prev_video_name, predicted_video_name):
         """计算课程内的相对视频位置的距离评分"""
         score = 0
-        is_next_video = False  # 新增一个标志，判断是否找到了相邻视频
+        f_next_video = True  # 新增一个标志，判断是否需要单独找相邻视频
 
         for pred_course in predicted_courses:
             for prev_course in prev_courses:
@@ -206,7 +233,7 @@ class Metrics(object):
 
                             if distance == 1:
                                 score += 10  # 确保相邻视频加足够高的分数
-                                is_next_video = True  # 标记为找到了下一个视频
+                                f_next_video = False  # 标记为不需要再找下一个视频
                             elif distance == 2:
                                 score += 9
                             elif distance == 3:
@@ -216,13 +243,14 @@ class Metrics(object):
                         except ValueError:
                             continue
 
-        # 如果确实找到了相邻视频，可以在这里设置更高的优先级
-        if is_next_video:
-            score += 10  # 再次强化相邻视频的分数
+        # # 如果确实找到了相邻视频，可以在这里设置更高的优先级
+        # if f_next_video:
+        #     score += 10  # 再次强化相邻视频的分数
 
-        return score
+        return score, f_next_video
 
     # 在 get_top_k_predictions 中插入下一个视频到首位
+    @staticmethod
     def reorder_top_predictions(self, top20, scores_pro, next_video_id=None):
         """根据得分重新排序top20预测视频，优先将下一个视频插入到第一位"""
         scored_videos = sorted(((v, scores_pro[v]) for v in top20 if scores_pro[v] > 0), key=lambda x: x[1],
