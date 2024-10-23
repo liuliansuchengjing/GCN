017d930b1f37a88100e55ffc1e05af640d313ef9
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-10-23 23:40:35 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -409,7 +409,7 @@ class Metrics(object):
 
                         if shortest_path != float('inf'):
                             # print("(opt)shortest_path:", shortest_path)
-                            scores_opt[video] += (1 / (1 + shortest_path))*0.5                            
+                            scores_opt[video] += (1 / (1 + shortest_path))*0.75                            
 
             # 如果得分为0，将其标记为零分视频
             if scores_opt[video] == 0:
