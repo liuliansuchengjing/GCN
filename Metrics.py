8cb0650751051cc32dccb84928bf412e2465fc8c
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-10-30 17:14:06 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -469,7 +469,7 @@ class Metrics(object):
                         shortest_path = graph.get_shortest_path_length(concept, focus_concept, all_shortest_paths)
 
                         if shortest_path != float('inf') and shortest_path != 0:
-                            # print("(opt)shortest_path:", shortest_path)
+                            print("(opt)shortest_path:", shortest_path)
                             scores_opt[video] += (1 / (1 + shortest_path))
                             # print(f"distance between {concept} and {focus_concept}: {shortest_path} ")
 
