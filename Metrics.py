6dbfe195ee144ed1fb7ab10389104736c7d3d6c2
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-10-30 17:35:56 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -310,7 +310,7 @@ class Metrics(object):
 
             # 概念距离排序
             # print("initial_topk:", initial_topk)
-            if wc > 10 and d2 < 0.6:
+            if wc > 10 and d2 < 0.8:
                 focus_concepts = graph.find_focus_concept(prev_video_name)
                 opt_topk = self.optimize_topk_based_on_concept(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
             else:
@@ -450,8 +450,8 @@ class Metrics(object):
     def optimize_topk_based_on_concept(self, knowledge_graph, focus_concepts, sorted_topk, idx2u, graph, all_shortest_paths):
         # video_scores = {}  # 用于存储视频及其累计相关性得分
         zero_score_videos_set = set()  # 用于去重存储得分为0的视频
-        # scores_opt = {video_id: (20 - i) if i < 20 else 0 for i, video_id in enumerate(sorted_topk)}
-        scores_opt = {video_id: 0 for video_id in sorted_topk}
+        scores_opt = {video_id: (20 - i) if i < 20 else 0 for i, video_id in enumerate(sorted_topk)}
+        # scores_opt = {video_id: 0 for video_id in sorted_topk}
 
         for video in sorted_topk:
             video_name = idx2u[video]  # 获取视频名称
@@ -470,7 +470,7 @@ class Metrics(object):
 
                         if shortest_path != float('inf') and shortest_path != 0:
                             # print("(opt)shortest_path:", shortest_path)
-                            scores_opt[video] += (1 / (1 + shortest_path))
+                            scores_opt[video] += (1 / (1 + shortest_path))*0.5
                             # print(f"distance between {concept} and {focus_concept}: {shortest_path} ")
 
             # 如果得分为0，将其标记为零分视频
