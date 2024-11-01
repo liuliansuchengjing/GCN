47fbae8424bbea917824178b7b2896860adb6883
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-11-01 17:09:31 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -314,9 +314,9 @@ class Metrics(object):
             if wc > 2 or d2 > 1:
                 focus_concepts = graph.find_focus_concept(prev_video_name)
                 opt_topk = self.optimize_topk_based_on_concept1(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
-            elif d2 < 0.1:
-                focus_concepts = graph.find_focus_concept(prev_video_name)
-                opt_topk = self.optimize_topk_based_on_concept2(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
+            # elif d2 < 0.1:
+            #     focus_concepts = graph.find_focus_concept(prev_video_name)
+            #     opt_topk = self.optimize_topk_based_on_concept2(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
             else:
                 opt_topk =list(initial_topk)
             
@@ -472,7 +472,7 @@ class Metrics(object):
                         # shortest_path = graph.direct_get_shortest_path_length(concept, focus_concept, concept_graph)
                         shortest_path = graph.get_shortest_path_length(concept, focus_concept, all_shortest_paths)
 
-                        if shortest_path != float('inf') and shortest_path != 2:                            
+                        if shortest_path != float('inf'):                            
                             # print("(opt)shortest_path:", shortest_path)
                             scores_opt[video] += (1 / (1 + shortest_path))
                             # scores_opt[video] += 0.22
