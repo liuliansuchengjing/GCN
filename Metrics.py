f35a92d85c66231f28a07d52897e30b141cd3bdb
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-11-07 11:39:17 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -505,7 +505,7 @@ class Metrics(object):
     def optimize_topk_based_on_concept2(self, knowledge_graph, focus_concepts, sorted_topk, idx2u, graph, all_shortest_paths):
         # video_scores = {}  # 用于存储视频及其累计相关性得分
         zero_score_videos_set = set()  # 用于去重存储得分为0的视频
-        scores_opt = {video_id: (20 - i) if i < 20 else 0 for i, video_id in enumerate(sorted_topk)}
+        scores_opt = {video_id: (40 - i) if i < 40 else 0 for i, video_id in enumerate(sorted_topk)}
         # scores_opt = {video_id: 0 for video_id in sorted_topk}
 
         for video in sorted_topk:
