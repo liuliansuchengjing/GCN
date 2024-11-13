c9e49bf002aafeb024c5edc3a1764cf6350b3aec
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-11-13 16:14:51 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -643,8 +643,10 @@ class Metrics(object):
         final_scores = {video: limited_video_scores.get(video, 0) + additional_scores.get(video, 0) for video in topk}
         final_scores = sorted([(video, score) for video, score in final_scores.items()], key=lambda x: x[1], reverse=True)
         # final_topk = [video for video, _ in sorted(final_scores.items(), key=lambda x: x[1], reverse=True)]
+        for video, score in top5_videos:
+            print(f"top5_videos: Course: {video}, Score: {score}")
         for video, score in final_scores:
-            print(f"Course: {video}, Score: {score}")
+            print(f"final_scores: Course: {video}, Score: {score}")
         # 提取排序后的视频ID
         final_topk = [video for video, score in final_scores]
 
