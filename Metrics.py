55be4ec22b7f9d05e83301f6f8343c3fe80b1d8c
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-11-22 15:49:33 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -447,8 +447,17 @@ class Metrics(object):
                                 key=lambda x: x[1], reverse=True)
 
         highscore_videos = [video for video, score in optimized_topk if score > 4]
+        mediumscore_videos = [video for video, score in optimized_topk if (score > 2 and score < 5)]
         # limited_video_scores = {video: video_scores[video] if video in top5_videos else 0 for video in topk}
-        limited_video_scores = {video: (scores_opt[video]-2) if video in highscore_videos else scores_opt[video] for video in topk}
+        # limited_video_scores = {video: (scores_opt[video]-2) if video in highscore_videos else scores_opt[video] for video in topk}
+        limited_video_scores = {
+            video: (
+                scores_opt[video] - 2 if video in highscore_videos
+                else scores_opt[video] - 1 if video in mediumscore_videos
+                else scores_opt[video]
+            )
+            for video in topk
+        }
         final_scores = {video: limited_video_scores.get(video, 0) + topk_scores.get(video, 0) for video in topk}
         scores_opt = sorted([(video, score) for video, score in final_scores.items()], key=lambda x: x[1],
                               reverse=True)
