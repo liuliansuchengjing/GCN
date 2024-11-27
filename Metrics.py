61737029713be4c29fab8c00f562c6054d3ba065
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-11-27 22:26:56 +0800

    Update Metrics.py

--- Metrics.py
+++ Metrics.py
@@ -255,18 +255,19 @@ class Metrics(object):
             scores_pro, f_next_video = self.score_nearby(initial_topk, y_p, idx2u, course_video_mapping, courses,
                                                               prev_courses)
 
-            # ------------------- 概念距离排序0
-            focus_concepts = graph.find_focus_concept(prev_video_name)
+            # # ------------------- 概念距离排序0
+            # focus_concepts = graph.find_focus_concept(prev_video_name)
 
-            if wc > 1 or d2 > 1 :
-                score_opt = self.optimize_topk_based_on_concept1(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
-                # sorted_topk = self.reorder_top_predictions(initial_topk, score_opt)
-                score = self.merge_scores(score_opt, scores_pro)
+            # if wc > 1 or d2 > 1 :
+            #     score_opt = self.optimize_topk_based_on_concept1(knowledge_graph, focus_concepts, initial_topk, idx2u, graph, all_shortest_paths)
+            #     # sorted_topk = self.reorder_top_predictions(initial_topk, score_opt)
+            #     score = self.merge_scores(score_opt, scores_pro)
 
-            else:
-                # sorted_topk = list(initial_topk)
-                score = scores_pro
+            # else:
+            #     # sorted_topk = list(initial_topk)
+            #     score = scores_pro
             # 根据得分重新排序topk
+            score = scores_pro
             sorted_topk = self.reorder_top_predictions(initial_topk, score)
 
             # -------------------单独使用一个分数排序
