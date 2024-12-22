2c47294b18d8209dddf6c392139c3b0d3da3ea86
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2024-12-22 13:11:05 +0800

    Update run.py

--- run.py
+++ run.py
@@ -195,6 +195,42 @@ def test_epoch(model, validation_data, graph, hypergraph_list, k_list=[5, 10, 20
 
     return scores
 
+def test3_epoch(model, validation_data, graph, hypergraph_list, k_list=[5, 10, 20]):
+    ''' Epoch operation in evaluation phase '''
+    model.eval()
+
+    scores = {}
+    for k in k_list:
+        scores['hits@' + str(k)] = 0
+        scores['map@' + str(k)] = 0
+
+    n_total_words = 0
+    with torch.no_grad():
+        for i, batch in enumerate(
+                validation_data):  # tqdm(validation_data, mininterval=2, desc='  - (Validation) ', leave=False):
+            # print("Validation batch ", i)
+            # prepare data
+            tgt, tgt_timestamp, tgt_idx = batch
+            y_gold = tgt[:, 1:].contiguous().view(-1).detach().cpu().numpy()
+
+            # forward
+            pred = model(tgt, tgt_timestamp, tgt_idx, graph, hypergraph_list)
+            y_pred = pred.detach().cpu().numpy()
+
+            scores_batch, scores_len = metric.compute_metric3(y_pred, y_gold, k_list)
+            n_total_words += scores_len
+            for k in k_list:
+                scores['hits@' + str(k)] += scores_batch['hits@' + str(k)] * scores_len
+                scores['map@' + str(k)] += scores_batch['map@' + str(k)] * scores_len
+                scores['NCDG@' + str(k)] += scores_batch['NCDG@' + str(k)] * scores_len
+
+    for k in k_list:
+        scores['hits@' + str(k)] = scores['hits@' + str(k)] / n_total_words
+        scores['map@' + str(k)] = scores['map@' + str(k)] / n_total_words
+        scores['NCDG@' + str(k)] = scores['NCDG@' + str(k)] / n_total_words
+
+    return scores
+
 
 def test_epoch_pro(model, validation_data, graph, hypergraph_list, k_list=[5, 10, 20]):
     ''' Epoch operation in evaluation phase '''
@@ -256,7 +292,8 @@ def test_model(MSHGAT, data_path):
     model.load_state_dict(torch.load(opt.save_path))
     model.cuda()
 
-    scores = test_epoch_pro(model, test_data, relation_graph, hypergraph_list, k_list=[1, 5, 10, 20, 25, 30, 35, 40])
+    # scores = test_epoch_pro(model, test_data, relation_graph, hypergraph_list, k_list=[1, 5, 10, 20, 25, 30, 35, 40])
+    scores = test_epoch3(model, test_data, relation_graph, hypergraph_list, k_list=[1, 5, 10, 20, 25, 30, 35, 40])
     # scores = test_epoch(model, test_data, relation_graph, hypergraph_list, k_list=[5, 10, 20, 30, 40, 50])
     print('  - (Test) ')
     for metric in scores.keys():
