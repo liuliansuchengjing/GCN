c9bbfcef3b189959255423abd676263a5130f02e
liuliansuchengjing <159008886+liuliansuchengjing@users.noreply.github.com>
2025-02-10 16:53:09 +0800

    Update HGAT.py

--- HGAT.py
+++ HGAT.py
@@ -279,6 +279,8 @@ class MSHGAT(nn.Module):
                 sub_cas[~temp] = 1
                 sub_cas = torch.einsum('ij,i->ij', sub_cas, input_idx)
                 sub_cas = F.embedding(sub_cas.cuda(), list(memory_emb_list.values())[ind - 1][1].cuda())
+                tensor = list(memory_emb_list.values())[ind - 1][1].cuda()
+                print("张量的维度:", tensor.shape)
                 sub_emb = F.embedding(cur.cuda(), list(memory_emb_list.values())[ind - 1][0].cuda())
                 sub_input = cur + sub_input
                 all_input = cur + all_input
